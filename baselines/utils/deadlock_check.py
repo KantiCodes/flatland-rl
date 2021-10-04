3422eafc3d2efa9d44a26054bad45c54fdfd1005
nimishsantosh107 <nimishsantosh107@icloud.com>
2021-10-04 23:21:56 +0530

    deadlock checked updated to statemachine specs

--- baselines/utils/deadlock_check.py
+++ baselines/utils/deadlock_check.py
@@ -1,8 +1,9 @@
 from flatland.core.grid.grid4_utils import get_new_position
-from flatland.envs.agent_utils import RailAgentStatus
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.step_utils.states import TrainState
 
 
-def check_if_all_blocked(env):
+def check_if_all_blocked(env: RailEnv):
     """
     Checks whether all the agents are blocked (full deadlock situation).
     In that case it is pointless to keep running inference as no agent will be able to move.
@@ -13,18 +14,16 @@ def check_if_all_blocked(env):
     # First build a map of agents in each position
     location_has_agent = {}
     for agent in env.agents:
-        if agent.status in [RailAgentStatus.ACTIVE, RailAgentStatus.DONE] and agent.position:
+        if agent.state.is_on_map_state():
             location_has_agent[tuple(agent.position)] = 1
 
     # Looks for any agent that can still move
     for handle in env.get_agent_handles():
         agent = env.agents[handle]
-        if agent.status == RailAgentStatus.READY_TO_DEPART:
+        if agent.state.is_off_map_state():
             agent_virtual_position = agent.initial_position
-        elif agent.status == RailAgentStatus.ACTIVE:
+        elif agent.state.is_on_map_state():
             agent_virtual_position = agent.position
-        elif agent.status == RailAgentStatus.DONE:
-            agent_virtual_position = agent.target
         else:
             continue
 
@@ -34,8 +33,7 @@ def check_if_all_blocked(env):
         for branch_direction in [(orientation + i) % 4 for i in range(-1, 3)]:
             if possible_transitions[branch_direction]:
                 new_position = get_new_position(agent_virtual_position, branch_direction)
-
-                if new_position not in location_has_agent:
+                if new_position not in location_has_agent.keys():
                     return False
 
     # No agent can move at all: full deadlock!
